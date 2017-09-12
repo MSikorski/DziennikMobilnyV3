7240a7cdb3462123ff51aa92fea97865d5c065b1
Mateusz Sikorski <mateuszsikorski110@gmail.com>
2017-09-12 11:26:44 +0200

    v01

---
+++ DziennikMobilny/src/v01/LoginController.java
@@ -0,0 +1,19 @@
+package v01;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+
+@Controller
+public class LoginController {
+	
+	@RequestMapping("/logowanie")
+	public String showLoginPage() {
+		return "login";
+	}
+	
+	@RequestMapping("/processForm")
+	public String processForm() {
+		return null;
+	}
+}
